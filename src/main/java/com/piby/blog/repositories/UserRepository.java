557d1038aa304ea3cc40b29937b36580a5105f35
Marco Martini <Marco Martini>
2016-12-05 13:41:24 +0100

    Adding derived query example

---
+++ src/main/java/com/piby/blog/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.piby.blog.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.piby.blog.entities.User;
+
+
+@RepositoryRestResource
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+	User findByName(@Param("name")String name);
+	Iterable<User> findByNameOrderByAgeAsc(@Param("name") String name);
+}
