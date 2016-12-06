471f1571b65dcc6da3f1cde0dbf354e69e329d29
Lorenzo D'Isidoro <lorenzo.disidoro@gmail.com>
2016-12-06 17:26:33 +0100

    b

--- src/main/java/com/piby/blog/PibyBlogApplication.java
+++ src/main/java/com/piby/blog/PibyBlogApplication.java
@@ -1,15 +1,7 @@
 package com.piby.blog;
 
-import com.piby.blog.repositories.CategoryRepository;
-import com.piby.blog.repositories.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @SpringBootApplication
 public class PibyBlogApplication {
