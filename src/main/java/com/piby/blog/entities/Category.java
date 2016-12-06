2c7e4e3bc9af30eb506300769ad3cdad7122442e
Marco Martini <Marco Martini>
2016-12-06 15:11:00 +0100

    Pre pulling

--- src/main/java/com/piby/blog/entities/Category.java
+++ src/main/java/com/piby/blog/entities/Category.java
@@ -1,7 +1,5 @@
 package com.piby.blog.entities;
 
-import javafx.geometry.Pos;
-
 import javax.persistence.*;
 import java.util.List;
 
@@ -12,32 +10,27 @@ import java.util.List;
 @Table(name = "category")
 public class Category {
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String title;
-    private String description;
-    @ManyToOne
-    private User user;
-    @ManyToMany
-    @JoinTable(
-            name="category2project",
-            joinColumns=@JoinColumn(name="category_id", referencedColumnName="id"),
-            inverseJoinColumns=@JoinColumn(name="post_id", referencedColumnName="id"))
-    private List<Post> posts;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
+	private String description;
+	@ManyToOne
+	private User user;
+	@ManyToMany
+	@JoinTable(name = "category2project", joinColumns = @JoinColumn(name = "category_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "post_id", referencedColumnName = "id"))
+	private List<Post> posts;
 
+	public Category(String title, String description, User user, List<Post> posts) {
+		this.title = title;
+		this.description = description;
+		this.user = user;
+		this.posts = posts;
+	}
 
-    public Category(String title, String description, User user, List<Post> posts) {
-        this.title = title;
-        this.description = description;
-        this.user = user;
-        this.posts = posts;
-    }
-
-
-    @Override
-    public String toString() {
-        return this.title + "(" + this.posts.size() + ")";
-    }
-
+	@Override
+	public String toString() {
+		return this.title + "(" + this.posts.size() + ")";
+	}
 
 }
