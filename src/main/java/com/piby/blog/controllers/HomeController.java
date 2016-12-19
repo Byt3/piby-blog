9322dc39ac671daaaa4b36b8342177bbe2dccacd
Lorenzo D'Isidoro <lorenzo.disidoro@gmail.com>
2016-12-19 11:05:34 +0100

    fix1

--- src/main/java/com/piby/blog/controllers/HomeController.java
+++ src/main/java/com/piby/blog/controllers/HomeController.java
@@ -42,24 +42,8 @@ public class HomeController {
         this.categoryRepository = categoryRepository;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model, Pageable pageable) {
-        final Iterable<Post> posts = postRepository.findAll();
-        final Iterable<Category> categories = categoryRepository.findAll();
-        model.addAttribute("posts", posts);
-        model.addAttribute("categories", categories);
-        return "index";
-    }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/view/new_post")
-    public String getUser(Model model) {
-        final Iterable<User> users = userRepository.findAll();
-        final Iterable<Category> categories = categoryRepository.findAll();
-        model.addAttribute("users", users);
-        model.addAttribute("categories", categories);
-        return "newPost";
-    }
-
+    // TODO: gestire
     @RequestMapping(method = RequestMethod.POST, value = "/api/posts",
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
             produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
@@ -73,46 +57,5 @@ public class HomeController {
         return new ResponseEntity<Post>(post, HttpStatus.OK);
 
     }
-
-//    @RequestMapping(method = RequestMethod.POST, value = "/api/post")
-//    public ResponseEntity<Void> saveData(@RequestBody Post post, UriComponentsBuilder ucBuilder){
-//        System.out.println("Creating User "+post.getTitle());
-//
-//        if(postRepository.findByTitle(post.getTitle())) {
-//            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
-//        }
-//
-//        postRepository.save(post);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setLocation(ucBuilder.path("/user/{id}").buildAndExpand(post.getId()).toUri());
-//        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
-//
-//    }
-
-//        @RequestMapping(method = RequestMethod.POST, value = BASE_PATH)
-//        @ResponseBody
-//        public ResponseEntity<?> createFile(@RequestParam("file") MultipartFile file, HttpServletRequest servletRequest) {
-//            try {
-//                imageService.createImage(file);
-//                final URI locationUri = new URI( servletRequest.getRequestURL().toString() + "/" )
-//                        .resolve(file.getOriginalFilename() + "/raw");
-//    //            return  ResponseEntity.created(request.getURI().resolve(file.getOriginalFilename() + "/raw"))
-//    //            .body("Success, file upload " + file.getOriginalFilename());
-//                System.out.println(date + " [POST] /images/"+file.getOriginalFilename());
-//                return  ResponseEntity.created(locationUri)
-//                        .body("Success, file upload " + file.getOriginalFilename());
-//
-//            } catch (IOException e) {
-//                return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                        .body("Filed to upload: " + file.getOriginalFilename() + " : " + e.getMessage());
-//            } catch (URISyntaxException e) {
-//                e.printStackTrace();
-//                return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                        .body("Filed URI Location: " + file.getOriginalFilename() + " : " + e.getMessage());
-//            }
-//        }
-
-
 
 }
---
+++ src/main/java/com/piby/blog/utility/ResponseParser.java
@@ -0,0 +1,89 @@
+package com.piby.blog.utility;
+
+import com.sun.jmx.snmp.Timestamp;
+import net.minidev.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * @author      Lorenzo D'Isidoro <lorenzo.disidoro@gmail.com>
+ * @version     1.2
+ * @since       1.0
+ */
+public class ResponseParser {
+
+    public ResponseParser() {}
+
+    /**
+     * Structure the response ResponseEntity
+     *
+     * @param HttpMethod (GET, DELETE, ...)
+     * @param isSuccess utility boolean for success
+     * @param data data to return
+     * @param activeLog utility to enable action log in console
+     *
+     * @return ResponseEntity to be used directly in the controller
+     */
+    public ResponseEntity<?> generateResponseEntity(String HttpMethod, boolean isSuccess, Object data, boolean activeLog) {
+
+        HttpStatus status;
+        String message;
+
+        switch (HttpMethod) {
+            case "GET":
+                message = isSuccess ? "Entity found" : "Entity not found";
+                status = isSuccess ? HttpStatus.FOUND : HttpStatus.NOT_FOUND;
+                if (activeLog) { System.out.println("[" + HttpMethod + "] " + message + " - [HttpStatus] " + status); }
+                return ResponseEntity.status(status)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(this.generateResponseBodyJsonObj(message, isSuccess, data, status));
+            case "POST":
+                message = isSuccess ? "Entity created" : "Entity not created";
+                status = isSuccess ? HttpStatus.CREATED : HttpStatus.NOT_ACCEPTABLE;
+                if (activeLog) { System.out.println("[" + HttpMethod + "] " + message + " - [HttpStatus] " + status); }
+                return ResponseEntity.status(status)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(this.generateResponseBodyJsonObj(message, isSuccess, data, status));
+            case "PUT":
+            case "PATCH":
+                message = isSuccess ? "Entity updated" : "Entity not updated";
+                status = isSuccess ? HttpStatus.OK : HttpStatus.NOT_MODIFIED;
+                if (activeLog) { System.out.println("[" + HttpMethod + "] " + message + " - [HttpStatus] " + status); }
+                return ResponseEntity.status(status)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(this.generateResponseBodyJsonObj(message, isSuccess, data, status));
+            default:
+                message = "HttpMethod not matched in Class ResponseParser <ResponseEntity()>";
+                status = HttpStatus.HTTP_VERSION_NOT_SUPPORTED;
+                if (activeLog) { System.out.println("[" + HttpMethod + "] " + message + " - [HttpStatus] " + status); }
+                return ResponseEntity.status(HttpStatus.HTTP_VERSION_NOT_SUPPORTED)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(this.generateResponseBodyJsonObj(message, false, new Object(), status));
+        }
+
+    }
+
+
+    /**
+     * Structure the response body
+     *
+     * @param message utility string message
+     * @param isSuccess utility boolean for success
+     * @param data data to return
+     *
+     * @return JSON Object response
+     */
+    public JSONObject generateResponseBodyJsonObj(String message, boolean isSuccess, Object data, HttpStatus status) {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        JSONObject response = new JSONObject();
+        response.put("error", !isSuccess);
+        response.put("message", message);
+        response.put("status", status);
+        response.put("timestamp", timestamp.getDate());
+        response.put("data", data);
+        return response;
+    }
+
+
+}
